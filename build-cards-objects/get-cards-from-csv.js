f791a019c9f7b890168f4cfbaf38b4bfaa281a35
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-12-23 14:30:18 -0500

    Refactor build-cards-object.js
    I'm mainly doing this so I get have a separate file
    (get-cards-from-csv.js) that I can reuse for other operations
    such as downloading card images.
    
    Coming up: download-card-images.js :)

---
+++ build-cards-objects/get-cards-from-csv.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const CSV_FILE_PATH = 'cards.csv';
+
+function csvRowToCard(row) {
+  return {
+    id: row.id,
+    name: row.name,
+    description: row.desc,
+    attack: parseInt(row.atk),
+    defense: parseInt(row.def),
+    level: parseInt(row.level),
+    type: row.type, // Examples: Flip Effect Monster, Trap Card, Fusion Monster, Spell Card
+    attribute: row.attribute, // Examples: WATER, DARK, EARTH
+    race: row.race, // Examples: Zombie, Fiend, Machine
+    archectype: row.archectype, // Examples: Elemental HERO, Umi, Gravekeeper's, Harpie
+    imageUrl: row.image_url,
+    treatedAs: row.treated_as,
+    hasEffect: row.has_effect === '1',
+  };
+}
+
+function isBeforeYear(yyyyMmDd, year) {
+  // Return false if yyyyMmDd is not a valid date
+  if (!yyyyMmDd) {
+    return false;
+  }
+  const date = new Date(yyyyMmDd);
+  return date.getFullYear() < year;
+}
+
+function getCardsBeforeYear(callback, releaseYearMax) {
+  const cards = [];
+  fs.createReadStream(CSV_FILE_PATH)
+    .pipe(csv())
+    .on('data', (row) => {
+      const releaseDate = row.ocg_date || row.tcg_date;
+      // Synchro cards were released in 2008 🤮
+      if (isBeforeYear(releaseDate, releaseYearMax)) {
+        cards.push(csvRowToCard(row));
+      }
+    })
+    .on('end', () => {
+      console.log(`Got ${cards.length} cards.`);
+      if (typeof callback === 'function') {
+        warnAboutInvalidities(cards);
+        callback(cards);
+      }
+    });
+}
+
+function getCardsBefore2008(callback) {
+  getCardsBeforeYear(callback, 2008);
+}
+
+function warnAboutInvalidities(cards) {
+  console.log(`Checking for duplicate IDs.`);
+  const idsMaps = new Map();
+  for (const card of cards) {
+    if (idsMaps.has(card.id)) {
+      console.warn(`Duplicate id: ${card.id}`);
+    }
+    idsMaps.set(card.id, card);
+  }
+}
+
+module.exports = { getCardsBefore2008 };
