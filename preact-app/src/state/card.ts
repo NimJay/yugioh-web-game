6d84ae98a59ff4f226bfabb971158fd0cffdf8d8
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-12-27 23:58:41 -0500

    Updare card.ts
    Technically, this should have been included in commit 459c285db9ec343537473fbd140dad96755f11ef ("Implement Pack buying (buyCardPack)").

--- preact-app/src/state/card.ts
+++ preact-app/src/state/card.ts
@@ -1,4 +1,10 @@
+import { cards } from "./cards";
+
 interface Card {
+  // id is not a unique identifier for a card.
+  // The unique identifier is the combination of id and rarity.
+  // For instance, the "Ultra Rare" version of a card has the
+  // same id as the "Common" version of the same card.
   id: string;
   name: string;
   description: string;
@@ -11,6 +17,16 @@ interface Card {
   imageUrl: string;
   treatedAs: string;
   hasEffect: boolean;
+  // If undefined, assume 'Common' rarity
+  rarity?: 'Common' // No shine
+    | 'Rare' // Shiny name
+    | 'Super Rare' // Shiny image
+    | 'Ultra Rare' // Shiny image and shiny name
+    | 'Secret Rare'; // Extra shiny image and shiny name
 }
 
-export { Card };
+function getCommonCardById(cardId: string): Card {
+  return cards.find(card => card.id === cardId);
+}
+
+export { Card, getCommonCardById };
