d88c779f4584b1dd843a4ebd8d52cf02e01598a8
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-12-28 21:44:23 -0500

    Fix bug in getAllCardsInPack()

--- preact-app/src/state/pack.ts
+++ preact-app/src/state/pack.ts
@@ -23,27 +23,27 @@ function getAllCardsInPack(packName: string): Card[] {
   for (const cardId of pack.commonCardIds) {
     const card = getCommonCardById(cardId);
     card.rarity = 'Common';
-    allCardsInPack.push();
+    allCardsInPack.push(card);
   }
   for (const cardId of pack.rareCardIds) {
     const card = getCommonCardById(cardId);
     card.rarity = 'Rare';
-    allCardsInPack.push();
+    allCardsInPack.push(card);
   }
   for (const cardId of pack.superRareCardIds) {
     const card = getCommonCardById(cardId);
     card.rarity = 'Super Rare';
-    allCardsInPack.push();
+    allCardsInPack.push(card);
   }
   for (const cardId of pack.ultraRareCardIds) {
     const card = getCommonCardById(cardId);
     card.rarity = 'Ultra Rare';
-    allCardsInPack.push();
+    allCardsInPack.push(card);
   }
   for (const cardId of pack.secretRareCardIds) {
     const card = getCommonCardById(cardId);
     card.rarity = 'Secret Rare';
-    allCardsInPack.push();
+    allCardsInPack.push(card);
   }
   return allCardsInPack;
 }
